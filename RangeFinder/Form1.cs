3125cf28056cc376fd310734b7934341c654b7fa
Ben Liddle <porkchopdt@gmail.com>
2018-10-08 19:51:14 +0100

    Working with test data

--- RangeFinder/Form1.cs
+++ RangeFinder/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,21 +16,20 @@ namespace RangeFinder
 
     public partial class Form1 : Form
     {
-        
+
         public Form1()
         {
             InitializeComponent();
-            
+
 
         }
-        
-        private void ImportUserCsv_click(object sender, System.EventArgs e)
-        {//Instantiate two lists on button click
-            List<long> RangeOutList = new List<long>();
-            List<User>UserOutList = new List<User>();
-            List<User>fullOutList = new List<User>();
 
-            List<KeyValuePair<string,string>> finalOut = new List<KeyValuePair<string, string>>();
+        private void ImportUserCsv_click(object sender, EventArgs e)
+        {//Instantiate three lists on button click
+            List<long> rangeOutList = new List<long>();
+            List<User> userOutList = new List<User>();
+            List<User> finalOut;
+            List<User> fullOutList = new List<User>();
 
             //check for data in the two boxes - if either is null, prompt for a filepath
 
@@ -39,17 +39,17 @@ namespace RangeFinder
 
                 foreach (var rNum in reader.IntCsvList)
                 {
-                   var _range = new Range(rNum);
-                    foreach (var _rNum in _range.RangeList)
+                    var range = new Range(rNum);
+                    foreach (var _rNum in range.RangeList)
                     {
-                        RangeOutList.Add(_rNum);
+                        rangeOutList.Add(_rNum);
                     }
                 }
 
                 foreach (var rUse in reader.StrCsvList)
                 {
-                    var _user = new User(rUse.Key,rUse.Value,false);
-                    UserOutList.Add(_user);
+                    var user = new User(rUse.Key, rUse.Value, false);
+                    userOutList.Add(user);
                 }
 
                 //foreach (var user in reader.StrCsvList)
@@ -70,32 +70,11 @@ namespace RangeFinder
                 //    }
                 //}
 
-                foreach (var x in RangeOutList)
-                {
-                    var unUser = new User("Unassigned",x,false);
-                    
-                    foreach (var user in UserOutList)
-                    {
-                        if (user.PhoneNumber == unUser.PhoneNumber && unUser.Listed == false && user.Listed == false)
-                        {
-                            fullOutList.Add(user);
-                            user.Listed = true;
-                            /*fullOutList.Add(unUser)*/;
-                          
-                        }
-                        else if (user.PhoneNumber != unUser.PhoneNumber && unUser.Listed == false && user.Listed == false)
-                        {
-                          fullOutList.Add(unUser);
-                            unUser.Listed = true;
-                            
-                        }
-                        else { break;}
-                    }
-                }
+                finalOut = ListModulo(rangeOutList, userOutList, fullOutList);
 
                 string outfile = string.Join
                 (
-                    Environment.NewLine, fullOutList.Select(d => d.UserName + "," + d.PhoneNumber)
+                    Environment.NewLine, finalOut.Select(d => d.UserName + "," + d.PhoneNumber)
                 );
                 System.IO.File.WriteAllText("C: \\Users\\Ben.Liddle\\Desktop\\Scripting\\rangetest.csv", outfile);
             }
@@ -105,7 +84,7 @@ namespace RangeFinder
             }
         }
 
-        
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -121,19 +100,19 @@ namespace RangeFinder
 
         private void UserListBrowser_Click(object sender, EventArgs e)
         {
-            DialogResult result = this.openFileDialog1.ShowDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
             if (result == DialogResult.OK)
             {
-                this.UserListPath.Text = this.openFileDialog1.FileName;
+                UserListPath.Text = openFileDialog1.FileName;
             }
         }
 
         private void RangeListBrowser_Click(object sender, EventArgs e)
         {
-            DialogResult result = this.openFileDialog1.ShowDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
             if (result == DialogResult.OK)
             {
-                this.RangeListPath.Text = this.openFileDialog1.FileName;
+                RangeListPath.Text = openFileDialog1.FileName;
             }
         }
 
@@ -141,5 +120,87 @@ namespace RangeFinder
         {
 
         }
+
+        public List<User> ListModulo(List<long> rList, List<User> uList, List<User> outList)
+        {
+            if (uList == null)
+            {
+                throw new ArgumentNullException(nameof(uList));
+            }
+
+
+
+            //foreach (var x in rList)
+            //{
+            //    var unUser = new User("Unassigned", x, false);
+
+            //    foreach (var user in uList)
+            //    {
+            //        if (user.PhoneNumber == unUser.PhoneNumber && unUser.Listed == false && user.Listed == false)
+            //        {
+            //           outList.Add(user);
+            //            user.Listed = true;
+            //            /*fullOutList.Add(unUser)*/
+            //        }
+            //        else if (user.PhoneNumber != unUser.PhoneNumber && unUser.Listed == false && user.Listed == false)
+            //        {
+            //            outList.Add(unUser);
+            //            unUser.Listed = true;
+
+            //        }
+            //        else { break; }
+            //    }
+            //}
+
+            foreach (var user in uList)
+            {
+                outList.Add(user);
+                user.Listed = true;
+
+                foreach (var x in rList)
+                {
+                    bool AlreadyListed = false;
+                    var unUser = new User("Unassigned", x, false);
+                    if (user.PhoneNumber != unUser.PhoneNumber && !Listed(outList,AlreadyListed,unUser))
+                    {
+                        var y = existenceCheck(unUser,uList);
+                        if (y) continue;
+                        unUser.Listed = true;
+                        outList.Add(unUser);
+
+                    }
+
+                }
+            }
+
+            return outList;
+        }
+
+        private bool existenceCheck(User user, List<User> list)
+        {
+            bool existing = false;
+            foreach (var u in list)
+            {
+                if (u.PhoneNumber == user.PhoneNumber)
+                {
+                    existing = true;
+                }
+            }
+
+            return existing;
+        }
+
+        bool Listed(List<User> list, bool alreadyListed, User user)
+        {
+            
+            
+                foreach (var entry in list)
+                {
+                    if (entry.PhoneNumber == user.PhoneNumber) { alreadyListed = true;}
+                }
+           
+            return alreadyListed;
+        }
+
     }
 }
